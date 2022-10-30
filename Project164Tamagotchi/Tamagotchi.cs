726f5878a474bdd89b39b164c3efa7e1d0910219
ThatgirlSipho <mkhefasipho@gmail.com>
2022-10-30 20:58:26 +0200

    Added the Tamagotchi

---
+++ Project164Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project164Tamagotchi
+{
+    internal class Tamagotchi
+    {
+        // Data Members
+        private int mSleep;
+        private int mHealth;
+        private int mCredit;
+        private string mCharacter;
+
+        //Constructor 1
+        public Tamagotchi(int sleep, int health, int credit, string character)
+        {
+            mSleep = sleep;
+            mHealth = health;
+            mCredit = credit;
+            mCharacter = character;
+        }
+
+        // Constructor 2
+        public Tamagotchi()
+        {
+            mSleep = 100;
+            mHealth = 0;
+            mCredit = 0;
+        }
+
+        //Properties 
+        public int Sleep
+        {
+            get { return mSleep; }
+            set { mSleep = value; }
+        }
+        public int Health
+        {
+            get { return mHealth; }
+            set { mHealth = value; }
+        }
+        public string Character
+        {
+            get { return mCharacter; }
+            set { mCharacter = value; }
+        }
+        public int Credit
+        {
+            get { return mCredit; }
+            set { mCredit = value; }
+        }
+
+        public int Happiness()
+        {
+            int happiness = mCredit + mHealth + mSleep;
+            happiness = happiness / 3;
+
+            return happiness;
+        }
+
+    }
+}
