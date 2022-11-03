f904d0b9f6208efc9cdb9075fbf1ffffebeb7ef9
ThatgirlSipho <mkhefasipho@gmail.com>
2022-11-03 12:38:37 +0200

    Added the sleep button along with its methods.

--- Project164Tamagotchi/Home.Designer.cs
+++ Project164Tamagotchi/Home.Designer.cs
@@ -32,6 +32,8 @@
             this.btnSleep = new System.Windows.Forms.Button();
             this.timerSleep = new System.Windows.Forms.Timer(this.components);
             this.timerAwake = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSleep = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSleep
@@ -44,16 +46,45 @@
             this.btnSleep.UseVisualStyleBackColor = true;
             this.btnSleep.Click += new System.EventHandler(this.btnSleep_Click);
             // 
+            // timerSleep
+            // 
+            this.timerSleep.Tick += new System.EventHandler(this.timerSleep_Tick);
+            // 
+            // timerAwake
+            // 
+            this.timerAwake.Tick += new System.EventHandler(this.timerAwake_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(493, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sleep Level:";
+            // 
+            // lblSleep
+            // 
+            this.lblSleep.AutoSize = true;
+            this.lblSleep.Location = new System.Drawing.Point(563, 279);
+            this.lblSleep.Name = "lblSleep";
+            this.lblSleep.Size = new System.Drawing.Size(10, 13);
+            this.lblSleep.TabIndex = 2;
+            this.lblSleep.Text = ".";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSleep);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSleep);
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,5 +93,7 @@
         private System.Windows.Forms.Button btnSleep;
         private System.Windows.Forms.Timer timerSleep;
         private System.Windows.Forms.Timer timerAwake;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSleep;
     }
 }
--- Project164Tamagotchi/Home.cs
+++ Project164Tamagotchi/Home.cs
@@ -17,6 +17,7 @@ namespace Project164Tamagotchi
             InitializeComponent();
         }
         public Tamagotchi pet = new Tamagotchi(); // Tamagotchi object that can be accessed in all forms
+        int sleep = 0;
 
         private void Home_Load(object sender, EventArgs e)
         {
@@ -26,6 +27,40 @@ namespace Project164Tamagotchi
         private void btnSleep_Click(object sender, EventArgs e)
         {
             timerSleep.Start();
+        }
+
+        private void timerSleep_Tick(object sender, EventArgs e)
+        {
+            //each tick is 2 seconds long and maximum sleep is 20 seconds
+            sleep += 10;
+            lblSleep.Text = Convert.ToString(sleep);
+            if (sleep == 100) //When sleep levels are full, pet wakes up
+            {
+                timerSleep.Stop();
+                timerAwake.Start();
+            }
+            // labelname.Text = pet.Character;
+        }
+
+        private void timerAwake_Tick(object sender, EventArgs e)
+        {
+            // each tick is 12 seconds and maximum awake time is 2 minutes 
+            if (timerSleep.Enabled)
+            {
+                timerAwake.Stop(); //making sure that these two do not run at the same time
+            }
+            else
+            {
+                timerAwake.Start();
+            }
+            sleep -= 10;
+            if (sleep == 0)
+            {
+                timerAwake.Stop();
+                MessageBox.Show(pet.Character + " is dead");
+                this.Close();
+            }
+            lblSleep.Text = Convert.ToString(sleep);
         }
     }
 }
