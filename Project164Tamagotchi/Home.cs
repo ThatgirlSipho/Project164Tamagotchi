cd81970688b2da87a6886e998f6b94925ba8846a
ThatgirlSipho <mkhefasipho@gmail.com>
2022-11-03 18:28:42 +0200

    Added the serialization methods

--- Project164Tamagotchi/Home.cs
+++ Project164Tamagotchi/Home.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,18 +18,33 @@ namespace Project164Tamagotchi
         {
             InitializeComponent();
         }
+
+
+
         public Tamagotchi pet = new Tamagotchi(); // Tamagotchi object that can be accessed in all forms
         int sleep = 0;
+
+
+
 
         private void Home_Load(object sender, EventArgs e)
         {
 
         }
 
+
+
+
+
+
         private void btnSleep_Click(object sender, EventArgs e)
         {
             timerSleep.Start();
         }
+
+
+
+
 
         private void timerSleep_Tick(object sender, EventArgs e)
         {
@@ -41,6 +58,10 @@ namespace Project164Tamagotchi
             }
             // labelname.Text = pet.Character;
         }
+
+
+
+
 
         private void timerAwake_Tick(object sender, EventArgs e)
         {
@@ -62,5 +83,39 @@ namespace Project164Tamagotchi
             }
             lblSleep.Text = Convert.ToString(sleep);
         }
+
+
+        public void WriteDataToFile(string petName, Tamagotchi digipet)
+        {
+            FileStream outFile = new FileStream(petName + ".ser", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(outFile, digipet);
+            outFile.Close();
+        }
+
+
+
+        public object ReadDataFromFile(string filepath)
+        {//deserialize file method 
+            object obj = null;
+            FileStream infile;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filepath))
+            {
+                infile = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+                obj = bf.Deserialize(infile);
+                infile.Close();
+            }
+            else
+            {
+                MessageBox.Show("File could not tbe found");
+            }
+
+            return obj;
+        }
+
+
+
+
     }
 }
