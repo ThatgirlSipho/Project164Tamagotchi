637f7b506431cd560ce450da54886e03efdb2e55
ThatgirlSipho <mkhefasipho@gmail.com>
2022-11-03 19:34:18 +0200

    Added theme

--- Project164Tamagotchi/Home.Designer.cs
+++ Project164Tamagotchi/Home.Designer.cs
@@ -39,12 +39,15 @@
             // 
             // btnSleep
             // 
-            this.btnSleep.Location = new System.Drawing.Point(493, 333);
+            this.btnSleep.BackColor = System.Drawing.Color.Sienna;
+            this.btnSleep.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSleep.ForeColor = System.Drawing.Color.YellowGreen;
+            this.btnSleep.Location = new System.Drawing.Point(451, 313);
             this.btnSleep.Name = "btnSleep";
-            this.btnSleep.Size = new System.Drawing.Size(75, 23);
+            this.btnSleep.Size = new System.Drawing.Size(125, 42);
             this.btnSleep.TabIndex = 0;
             this.btnSleep.Text = "Sleep";
-            this.btnSleep.UseVisualStyleBackColor = true;
+            this.btnSleep.UseVisualStyleBackColor = false;
             this.btnSleep.Click += new System.EventHandler(this.btnSleep_Click);
             // 
             // timerSleep
@@ -58,27 +61,34 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(493, 280);
+            this.label1.BackColor = System.Drawing.Color.YellowGreen;
+            this.label1.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(448, 279);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.Size = new System.Drawing.Size(100, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Sleep Level:";
             // 
             // lblSleep
             // 
             this.lblSleep.AutoSize = true;
+            this.lblSleep.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSleep.ForeColor = System.Drawing.Color.DodgerBlue;
             this.lblSleep.Location = new System.Drawing.Point(563, 279);
             this.lblSleep.Name = "lblSleep";
-            this.lblSleep.Size = new System.Drawing.Size(10, 13);
+            this.lblSleep.Size = new System.Drawing.Size(13, 17);
             this.lblSleep.TabIndex = 2;
             this.lblSleep.Text = ".";
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Ravie", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.DodgerBlue;
             this.lblName.Location = new System.Drawing.Point(273, 36);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.Size = new System.Drawing.Size(133, 43);
             this.lblName.TabIndex = 3;
             this.lblName.Text = "Name";
             // 
@@ -86,6 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblSleep);
