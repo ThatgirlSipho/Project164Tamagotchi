f61c59b795c4ebb2e05d9de8832e7e695e87cda8
ThatgirlSipho <mkhefasipho@gmail.com>
2022-11-03 20:06:21 +0200

    Added all the neccessay buttons and labels

--- Project164Tamagotchi/Home.Designer.cs
+++ Project164Tamagotchi/Home.Designer.cs
@@ -35,16 +35,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lblSleep = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
+            this.btnPantry = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPlay = new System.Windows.Forms.Label();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.lblHappiness = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSleep
             // 
             this.btnSleep.BackColor = System.Drawing.Color.Sienna;
             this.btnSleep.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSleep.ForeColor = System.Drawing.Color.YellowGreen;
-            this.btnSleep.Location = new System.Drawing.Point(451, 313);
+            this.btnSleep.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSleep.Location = new System.Drawing.Point(562, 357);
             this.btnSleep.Name = "btnSleep";
-            this.btnSleep.Size = new System.Drawing.Size(125, 42);
+            this.btnSleep.Size = new System.Drawing.Size(127, 42);
             this.btnSleep.TabIndex = 0;
             this.btnSleep.Text = "Sleep";
             this.btnSleep.UseVisualStyleBackColor = false;
@@ -64,18 +73,18 @@
             this.label1.BackColor = System.Drawing.Color.YellowGreen;
             this.label1.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label1.Location = new System.Drawing.Point(448, 279);
+            this.label1.Location = new System.Drawing.Point(595, 320);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.Size = new System.Drawing.Size(54, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Sleep Level:";
+            this.label1.Text = "Sleep:";
             // 
             // lblSleep
             // 
             this.lblSleep.AutoSize = true;
             this.lblSleep.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSleep.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblSleep.Location = new System.Drawing.Point(563, 279);
+            this.lblSleep.Location = new System.Drawing.Point(646, 320);
             this.lblSleep.Name = "lblSleep";
             this.lblSleep.Size = new System.Drawing.Size(13, 17);
             this.lblSleep.TabIndex = 2;
@@ -86,11 +95,110 @@
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Ravie", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblName.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblName.Location = new System.Drawing.Point(273, 36);
+            this.lblName.Location = new System.Drawing.Point(317, 27);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(133, 43);
             this.lblName.TabIndex = 3;
             this.lblName.Text = "Name";
+            // 
+            // btnPantry
+            // 
+            this.btnPantry.BackColor = System.Drawing.Color.Sienna;
+            this.btnPantry.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPantry.Location = new System.Drawing.Point(335, 357);
+            this.btnPantry.Name = "btnPantry";
+            this.btnPantry.Size = new System.Drawing.Size(127, 42);
+            this.btnPantry.TabIndex = 4;
+            this.btnPantry.Text = "Pantry";
+            this.btnPantry.UseVisualStyleBackColor = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Sienna;
+            this.btnPlay.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(108, 357);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(127, 42);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(144, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Play:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(360, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Health:";
+            // 
+            // lblPlay
+            // 
+            this.lblPlay.AutoSize = true;
+            this.lblPlay.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlay.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblPlay.Location = new System.Drawing.Point(186, 322);
+            this.lblPlay.Name = "lblPlay";
+            this.lblPlay.Size = new System.Drawing.Size(13, 17);
+            this.lblPlay.TabIndex = 8;
+            this.lblPlay.Text = ".";
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHealth.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblHealth.Location = new System.Drawing.Point(420, 322);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(13, 17);
+            this.lblHealth.TabIndex = 9;
+            this.lblHealth.Text = ".";
+            // 
+            // lblHappiness
+            // 
+            this.lblHappiness.AutoSize = true;
+            this.lblHappiness.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHappiness.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblHappiness.Location = new System.Drawing.Point(67, 56);
+            this.lblHappiness.Name = "lblHappiness";
+            this.lblHappiness.Size = new System.Drawing.Size(165, 86);
+            this.lblHappiness.TabIndex = 10;
+            this.lblHappiness.Text = "100";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Sienna;
+            this.btnSave.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(709, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(79, 35);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Sienna;
+            this.btnClose.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(709, 56);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(79, 35);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
             // 
             // Home
             // 
@@ -98,6 +206,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblHappiness);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.lblPlay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnPantry);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblSleep);
             this.Controls.Add(this.label1);
@@ -118,5 +235,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblSleep;
         private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnPantry;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPlay;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label lblHappiness;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
     }
 }
