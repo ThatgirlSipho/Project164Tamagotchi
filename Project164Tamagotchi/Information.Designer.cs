8d6c010df101e0a7173f33d2ffc82cf31c14432a
SharonKN <106258144+SharonKN@users.noreply.github.com>
2022-11-04 20:33:46 +0200

    Added design

--- Project164Tamagotchi/Information.Designer.cs
+++ Project164Tamagotchi/Information.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(873, 384);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2) Fruits and veg = R5\r\n1) Fats and oils = R10\r\n3) Proteins = R15\r\n4)Carbohydrate" +
+    "s = R20\r\nCan only purchase items with 30 points or more\r\n\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(341, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 64);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cost of Food Type:";
+            // 
+            // Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(1156, 495);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Ravie", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Information";
             this.Text = "Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
