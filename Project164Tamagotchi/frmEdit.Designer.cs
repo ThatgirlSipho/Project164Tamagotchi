abab263b75b8382d410786904f440849e729e43f
SharonKN <106258144+SharonKN@users.noreply.github.com>
2022-11-04 08:18:33 +0200

    edited edit form

--- Project164Tamagotchi/frmEdit.Designer.cs
+++ Project164Tamagotchi/frmEdit.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(978, 562);
+            this.Font = new System.Drawing.Font("Papyrus", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmEdit";
             this.Text = "frmEdit";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
