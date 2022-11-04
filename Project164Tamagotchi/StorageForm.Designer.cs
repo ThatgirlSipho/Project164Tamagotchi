895b539db6b5ad4c340670ffee1ae9f00bf71795
SharonKN <106258144+SharonKN@users.noreply.github.com>
2022-11-04 08:25:29 +0200

    Added serialization

--- Project164Tamagotchi/StorageForm.Designer.cs
+++ Project164Tamagotchi/StorageForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StorageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "StorageForm";
             this.Text = "StorageForm";
+            this.Load += new System.EventHandler(this.StorageForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Project164Tamagotchi/StorageForm.cs
+++ Project164Tamagotchi/StorageForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Project164Tamagotchi
 {
@@ -15,6 +18,37 @@ namespace Project164Tamagotchi
         public StorageForm()
         {
             InitializeComponent();
+        }
+
+        //serialization method to write data of food items to file
+        public void WriteDataToFile(string listName, BindingList<Food> foodItems)
+        {
+            FileStream outFile = new FileStream(listName + ".ser", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(outFile, foodItems);
+            outFile.Close();
+        }
+
+
+        //serialization method to read data of food from file
+        public void ReadDataFromFile(string listName, BindingList<Food> foodItems)
+        {
+            try
+            {
+                FileStream inFile = new FileStream(listName + ".ser", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                foodItems.Clear();
+                var tempList = (BindingList<Food>)bFormatter.Deserialize(inFile);
+                foreach (Food myObject in tempList)
+                { foodItems.Add(myObject); }
+                inFile.Close();
+            }
+            catch (FileNotFoundException)
+            { MessageBox.Show("The data file could not be found"); }
+        }
+        private void StorageForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
