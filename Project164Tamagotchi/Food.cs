79d1beef518a573472c512d35f12a2d59d06f258
SharonKN <106258144+SharonKN@users.noreply.github.com>
2022-11-04 20:46:45 +0200

    Added code to class

--- Project164Tamagotchi/Food.cs
+++ Project164Tamagotchi/Food.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Project164Tamagotchi
 {
-    internal class Food
+    [Serializable]
+    public class Food
     {
+        //data members
+        private string mName;
+        private string mType;
+
+
+        //properties
+        public string Name { get => mName; set => mName = value; }
+        public string Type { get => mType; set => mType = value; }
+
+
+
+
+        //default constructor
+        public Food()
+        {
+            mName = "";
+            mType = "";
+
+        }
+
+        //overloaded constructor
+        public Food(string Name, string Type, int Cost)
+        {
+            mName = Name;
+            mType = Type;
+
+        }
+
+
+
     }
 }
